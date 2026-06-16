96dcb3fafa3199f95a333f4468b93eada49b6170
Abod_Hares <133676503+abod-haes@users.noreply.github.com>
2026-06-16 15:02:38 +0300

    Add checkout cancel route

---
+++ src/app/checkout/cancel/page.tsx
@@ -0,0 +1,7 @@
+import { CheckoutCancelPage, generateCheckoutCancelMetadata } from "@/features/checkout/checkout-pages.component";
+
+export const generateMetadata = generateCheckoutCancelMetadata;
+
+export default function Page() {
+  return <CheckoutCancelPage />;
+}
