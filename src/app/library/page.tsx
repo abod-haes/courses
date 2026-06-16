ceb432c17e82bf731cad0dc2d516b3a35969b768
Abod_Hares <133676503+abod-haes@users.noreply.github.com>
2026-06-16 15:03:19 +0300

    Add student library route

---
+++ src/app/library/page.tsx
@@ -0,0 +1,7 @@
+import { LibraryPage, generateLibraryMetadata } from "@/features/checkout/checkout-pages.component";
+
+export const generateMetadata = generateLibraryMetadata;
+
+export default function Page() {
+  return <LibraryPage />;
+}
